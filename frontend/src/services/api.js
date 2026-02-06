7ffc5b607d5895baa8c971a46244fb7a79dadb2d
Afrah Mukadam <afrahmukadam@Afrahs-MacBook-Air.local>
2026-02-06 17:38:02 +0400

    Use Netlify environment API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://todo-backend-mnk3.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 // Attach token automatically
