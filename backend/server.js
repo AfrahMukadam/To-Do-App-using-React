4e402cb1d9afb007d0853049f2fcbb18d2151a18
Afrah Mukadam <afrahmukadam@Afrahs-MacBook-Air.local>
2026-02-06 16:57:30 +0400

    Prepare backend for Render deployment

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,14 @@ const authRoutes = require("./routes/authRoutes");
 const taskRoutes = require("./routes/taskRoutes");
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
@@ -25,9 +31,11 @@ mongoose
   .then(() => {
     console.log("MongoDB connected");
 
-    app.listen(5000, "127.0.0.1", () => {
-      console.log("Server running on port 5000");
-    });
+    
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
   })
   .catch((err) => {
     console.error("MongoDB connection error:", err);
